cde82bb0a9cbf1ca5000a3f28822a360ec7b9e3f
Diego Cintra <diegoffcintra@gmail.com>
2022-07-19 07:58:57 -0300

    Checkout

--- resources/views/billing/paymentSelect.blade.php
+++ resources/views/billing/paymentSelect.blade.php
@@ -5,6 +5,16 @@
     <div class="row">
         <div class="col-4">
             <div class="card mt-4">
+                <div class="card-body bg-white">
+                    <h3 class="text-center">Dados de Cadastro</h3>
+                    <hr>
+                    <span>Diego Cintra</span><br>
+                    <span>E-mail: diegoffcintra@gmail.com</span><br>
+                    <span>CNPJ: 36.771.571/0001-77</span><br>
+                    <span>Razão Social: Fishway Pesca LTDA</span>
+                </div>
+            </div>
+            <div class="card mt-4">
                 <div class="card-body bg-white p-3">
                     <h3 class="text-center">Resumo do pedido</h3>
                     <hr>
@@ -24,7 +34,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-8 float-right">
+        <div class="col-7 float-right">
             <div class="mt-3">
                 <div class="col">
                     <h2>Escolha o meio de pagamento</h2>
@@ -32,31 +42,47 @@
                 <div class="col mt-3">
                     <div class="card bg-white">
                         <div class="card-body">
-                            <span>Boleto Bancário</span>
-                            <a href="" class="float-right">
-                                <button class="btn btn-primary">Pagar</button>
-                            </a>
+                            <div class="col">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        <span>Boleto Bancário - <span>5% de Desconto</span></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- <div class="col">
+                                <a href="" class="">
+                                    <button class="btn btn-primary">Pagar</button>
+                                </a>
+                            </div> -->
                         </div>
                     </div>
                 </div>
                 <div class="col mt-3">
                     <div class="card bg-white">
                         <div class="card-body">
-                            <span>PIX </span>
-                            <span class="badge badge-success ml-2">Aprovação Imediata</span>
-                            <a href="" class="float-right">
-                                <button class="btn btn-primary">Pagar</button>
-                            </a>
+                            <div class="col">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        <span>PIX - <span>10% de Desconto</span></span>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col mt-3">
                     <div class="card bg-white">
                         <div class="card-body">
-                            <span>Cartão de Crédito </span>
-                            <a href="" class="float-right">
-                                <button class="btn btn-primary">Pagar</button>
-                            </a>
+                        <div class="col">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1">
+                                    <label class="form-check-label" for="exampleRadios1">
+                                        <span>Cartão de Crédito</span>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
